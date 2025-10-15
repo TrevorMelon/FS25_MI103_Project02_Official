81cddb390c3cccf3ec39295859074d98583aae19
TrevorMelon <moreho24@msu.edu>
2025-10-15 19:46:52 -0400

    Finished PLayerController Script
    added vertical movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     { 
         float horizontal = 0.0f;
-
+        float vertical = 0.0f;
         if (Keyboard.current.leftArrowKey.isPressed)
         {
 
@@ -29,11 +29,19 @@ public class PlayerController : MonoBehaviour
             horizontal = 1.0f;
 
         }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+        else if (Keyboard.current.upArrowKey.isPressed)
+        {  vertical = 1.0f;
+        }
         {
             Debug.Log(horizontal);
 
             Vector2 position = transform.position;
             position.x = position.x + 0.1f * horizontal;
+            position.y = position.y + 0.1f * vertical;
             transform.position = position;
         }
 
